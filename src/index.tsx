b128f9c1e699aa47de969b8fd41911d5070ca5c5
oren raz <oren@lusha.co>
2020-08-20 20:26:17 +0300

    add server and client

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// Import necessary dependencies
+import React from 'react'
+import ReactDOM from 'react-dom'
 
+// Import App component
+import App from './components/app'
+
+// Import styles
+import './css/index.css'
+
+// Import service workers
+import * as serviceWorker from './serviceWorker'
+
+// Render App component in the DOM
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+    , document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
